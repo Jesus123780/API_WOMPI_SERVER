ab7571e4eaa7c00ded6fe0daa9d7b648a09b8a54
Cristian <cristianjuvinao801@gmai.com>
2024-02-27 09:15:32 -0500

    feat: create husky files

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'standard-with-typescript'
+
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,34 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-// import wompi from '@wompi/api';
+'use strict'
+
+import express from 'express'
+import { type Request, type Response } from 'express'
+
+import bodyParser from 'body-parser'
+// import wompi from '@wompi/api'
 
 // Crear una instancia de Express
-const app = express();
+const app = express()
 
 // Middleware para analizar el cuerpo de las solicitudes entrantes como JSON
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // Ruta de prueba para verificar que la API está funcionando
 app.get('/', (req: Request, res: Response) => {
-  res.send('¡API de prueba funcionando!');
-});
+  res.send('¡API de prueba funcionando!')
+})
 
-// Ruta para procesar pagos utilizando la API de Wompi
-app.post('/pagos', async (req: Request, res: Response) => {
+app.post('/pagos', (req: Request, res: Response): void => {
   try {
-    // Aquí procesaremos el pago utilizando la API de Wompi
-    // Reemplaza este código con la lógica real para procesar el pago
-
-    // Por ahora, solo devolvemos un mensaje de éxito
-    res.json({ message: 'Pago procesado exitosamente' });
+    res.json({ message: 'Pago procesado exitosamente' })
   } catch (error) {
-    // Si ocurre algún error, devolvemos un mensaje de error
-    res.status(500).json({ error: 'Ocurrió un error al procesar el pago' });
+    res.status(500).json({ error: 'Ocurrió un error al procesar el pago' })
   }
-});
+})
 
 // Puerto en el que escucha el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+  console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
